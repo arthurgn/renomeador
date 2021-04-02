8a1f029adc5bb0f528a7965b7a38c6a2862bbbe5
Arthur Gomes <arthur.gn@hotmail.com>
2021-04-01 21:42:55 -0300

    versao 2

--- renomeador/frmrenomeador.cs
+++ renomeador/frmrenomeador.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace renomeador
@@ -18,6 +12,11 @@ namespace renomeador
             InitializeComponent();
 
             Load += Frmrenomeador_Load;
+
+
+            // teste de versao
+
+
         }
 
 
